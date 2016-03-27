58b1e205da3af2c2faac703be836b27627f71a84
Sai Pc <schand31@asu.edu>
2016-03-27 14:43:56 -0700

    added stuff to CRUD pages, edits etc

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160327052928) do
+ActiveRecord::Schema.define(version: 20160327213411) do
 
   create_table "tickets", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160327052928) do
     t.string   "email"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "phone"
   end
 
 end
